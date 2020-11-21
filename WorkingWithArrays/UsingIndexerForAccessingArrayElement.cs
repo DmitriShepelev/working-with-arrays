c11863de9a827f2ff75868b4a320b0244be16fa2
dmitri_shepelev <shepelev.dm@gmail.com>
2020-11-22 00:24:20 +0300

    completed

--- WorkingWithArrays/UsingIndexerForAccessingArrayElement.cs
+++ WorkingWithArrays/UsingIndexerForAccessingArrayElement.cs
@@ -1,217 +1,227 @@
-﻿using System;
-
-#pragma warning disable CA1062
+﻿#pragma warning disable CA1062
 
 namespace WorkingWithArrays
 {
     public static class UsingIndexerForAccessingArrayElement
     {
+#pragma warning disable S4136 // Method overloads should be grouped together
         public static int GetFirstArrayElement(int[] array)
+#pragma warning restore S4136 // Method overloads should be grouped together
         {
-            // TODO #2-1. Add the method implementation. The method should return a first element of the specified array.
-            throw new NotImplementedException();
+            // #2-1. Add the method implementation. The method should return a first element of the specified array.
+            return array[0];
         }
 
+#pragma warning disable S4136 // Method overloads should be grouped together
         public static int GetSecondArrayElement(int[] array)
+#pragma warning restore S4136 // Method overloads should be grouped together
         {
-            // TODO #2-2. Add the method implementation. The method should return a second element of the specified array.
-            throw new NotImplementedException();
+            // #2-2. Add the method implementation. The method should return a second element of the specified array.
+            return array[1];
         }
 
         public static int GetThirdArrayElement(int[] array)
         {
-            // TODO #2-3. Add the method implementation. The method should return a third element of the specified array.
-            throw new NotImplementedException();
+            // #2-3. Add the method implementation. The method should return a third element of the specified array.
+            return array[2];
         }
 
+#pragma warning disable S4136 // Method overloads should be grouped together
         public static int GetLastElement(int[] array)
+#pragma warning restore S4136 // Method overloads should be grouped together
         {
-            // TODO #2-4. Add the method implementation. The method should return a last element of the specified array.
+            // #2-4. Add the method implementation. The method should return a last element of the specified array.
             // Use Array.Length property to get an array length: https://docs.microsoft.com/en-us/dotnet/api/system.array.length
-            throw new NotImplementedException();
+            return array[^1];
         }
 
+#pragma warning disable S4136 // Method overloads should be grouped together
         public static int GetNextToLastElement(int[] array)
+#pragma warning restore S4136 // Method overloads should be grouped together
         {
-            // TODO #2-5. Add the method implementation. The method should return a next to last element of the specified array.
+            // #2-5. Add the method implementation. The method should return a next to last element of the specified array.
             // Use Array.Length property to get an array length: https://docs.microsoft.com/en-us/dotnet/api/system.array.length
-            throw new NotImplementedException();
+            return array[^2];
         }
 
+#pragma warning disable S4136 // Method overloads should be grouped together
         public static int GetNthArrayElement(int[] array, int n)
+#pragma warning restore S4136 // Method overloads should be grouped together
         {
-            // TODO #2-6. Add the method implementation. The method should return a Nth element of the specified array.
-            throw new NotImplementedException();
+            // #2-6. Add the method implementation. The method should return a Nth element of the specified array.
+            return array[n - 1];
         }
 
         public static bool GetFirstArrayElement(bool[] array)
         {
-            // TODO #2-7. Add the method implementation. The method should return a first element of the specified array.
-            throw new NotImplementedException();
+            // #2-7. Add the method implementation. The method should return a first element of the specified array.
+            return array[0];
         }
 
         public static bool GetSecondArrayElement(bool[] array)
         {
-            // TODO #2-8. Add the method implementation. The method should return a second element of the specified array.
-            throw new NotImplementedException();
+            // #2-8. Add the method implementation. The method should return a second element of the specified array.
+            return array[1];
         }
 
         public static bool GetSixthArrayElement(bool[] array)
         {
-            // TODO #2-9. Add the method implementation. The method should return a sixth element of the specified array.
-            throw new NotImplementedException();
+            // #2-9. Add the method implementation. The method should return a sixth element of the specified array.
+            return array[5];
         }
 
         public static bool GetLastElement(bool[] array)
         {
-            // TODO #2-10. Add the method implementation. The method should return a last element of the specified array.
+            // #2-10. Add the method implementation. The method should return a last element of the specified array.
             // Use Array.Length property to get an array length: https://docs.microsoft.com/en-us/dotnet/api/system.array.length
-            throw new NotImplementedException();
+            return array[^1];
         }
 
         public static bool GetNextToLastElement(bool[] array)
         {
-            // TODO #2-11. Add the method implementation. The method should return a next to last element of the specified array.
+            // #2-11. Add the method implementation. The method should return a next to last element of the specified array.
             // Use Array.Length property to get an array length: https://docs.microsoft.com/en-us/dotnet/api/system.array.length
-            throw new NotImplementedException();
+            return array[^2];
         }
 
         public static bool GetNthArrayElement(bool[] array, int n)
         {
-            // TODO #2-12. Add the method implementation. The method should return a Nth element of the specified array.
-            throw new NotImplementedException();
+            // #2-12. Add the method implementation. The method should return a Nth element of the specified array.
+            return array[n - 1];
         }
 
         public static string GetFirstArrayElement(string[] array)
         {
-            // TODO #2-13. Add the method implementation. The method should return a first element of the specified array.
-            throw new NotImplementedException();
+            // #2-13. Add the method implementation. The method should return a first element of the specified array.
+            return array[0];
         }
 
         public static string GetForthArrayElement(string[] array)
         {
-            // TODO #2-14. Add the method implementation. The method should return a forth element of the specified array.
-            throw new NotImplementedException();
+            // #2-14. Add the method implementation. The method should return a forth element of the specified array.
+            return array[3];
         }
 
         public static string GetLastElement(string[] array)
         {
-            // TODO #2-15. Add the method implementation. The method should return a last element of the specified array.
+            // #2-15. Add the method implementation. The method should return a last element of the specified array.
             // Use Array.Length property to get an array length: https://docs.microsoft.com/en-us/dotnet/api/system.array.length
-            throw new NotImplementedException();
+            return array[^1];
         }
 
         public static string GetNextToLastElement(string[] array)
         {
-            // TODO #2-16. Add the method implementation. The method should return a next to last element of the specified array.
+            // #2-16. Add the method implementation. The method should return a next to last element of the specified array.
             // Use Array.Length property to get an array length: https://docs.microsoft.com/en-us/dotnet/api/system.array.length
-            throw new NotImplementedException();
+            return array[^2];
         }
 
         public static char GetFirstArrayElement(char[] array)
         {
-            // TODO #2-17. Add the method implementation. The method should return a first element of the specified array.
-            throw new NotImplementedException();
+            // #2-17. Add the method implementation. The method should return a first element of the specified array.
+            return array[0];
         }
 
+#pragma warning disable S4136 // Method overloads should be grouped together
         public static char GetSeventhArrayElement(char[] array)
+#pragma warning restore S4136 // Method overloads should be grouped together
         {
-            // TODO #2-18. Add the method implementation. The method should return a seventh element of the specified array.
-            throw new NotImplementedException();
+            // #2-18. Add the method implementation. The method should return a seventh element of the specified array.
+            return array[6];
         }
 
         public static char GetLastElement(char[] array)
         {
-            // TODO #2-19. Add the method implementation. The method should return a last element of the specified array.
+            // #2-19. Add the method implementation. The method should return a last element of the specified array.
             // Use Array.Length property to get an array length: https://docs.microsoft.com/en-us/dotnet/api/system.array.length
-            throw new NotImplementedException();
+            return array[^1];
         }
 
         public static char GetNextToLastElement(char[] array)
         {
-            // TODO #2-20. Add the method implementation. The method should return a next to last element of the specified array.
+            // #2-20. Add the method implementation. The method should return a next to last element of the specified array.
             // Use Array.Length property to get an array length: https://docs.microsoft.com/en-us/dotnet/api/system.array.length
-            throw new NotImplementedException();
+            return array[^2];
         }
 
         public static double GetFirstArrayElement(double[] array)
         {
-            // TODO #2-21. Add the method implementation. The method should return a first element of the specified array.
-            throw new NotImplementedException();
+            // #2-21. Add the method implementation. The method should return a first element of the specified array.
+            return array[0];
         }
 
         public static double GetSeventhArrayElement(double[] array)
         {
-            // TODO #2-22. Add the method implementation. The method should return a seventh element of the specified array.
-            throw new NotImplementedException();
+            // #2-22. Add the method implementation. The method should return a seventh element of the specified array.
+            return array[6];
         }
 
         public static double GetLastElement(double[] array)
         {
-            // TODO #2-23. Add the method implementation. The method should return a last element of the specified array.
+            // #2-23. Add the method implementation. The method should return a last element of the specified array.
             // Use index from end operator: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/ranges-indexes
-            throw new NotImplementedException();
+            return array[^1];
         }
 
         public static double GetNextToLastElement(double[] array)
         {
-            // TODO #2-24. Add the method implementation. The method should return a next to last element of the specified array.
+            // #2-24. Add the method implementation. The method should return a next to last element of the specified array.
             // Use index from end operator: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/ranges-indexes
-            throw new NotImplementedException();
+            return array[^2];
         }
 
         public static float GetFirstArrayElement(float[] array)
         {
-            // TODO #2-25. Add the method implementation. The method should return a first element of the specified array.
-            throw new NotImplementedException();
+            // #2-25. Add the method implementation. The method should return a first element of the specified array.
+            return array[0];
         }
 
         public static float GetNinthArrayElement(float[] array)
         {
-            // TODO #2-26. Add the method implementation. The method should return a ninth element of the specified array.
-            throw new NotImplementedException();
+            // #2-26. Add the method implementation. The method should return a ninth element of the specified array.
+            return array[8];
         }
 
         public static float GetLastElement(float[] array)
         {
-            // TODO #2-27. Add the method implementation. The method should return a last element of the specified array.
+            // #2-27. Add the method implementation. The method should return a last element of the specified array.
             // Use index from end operator: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/ranges-indexes
-            throw new NotImplementedException();
+            return array[^1];
         }
 
         public static float GetNextToLastElement(float[] array)
         {
-            // TODO #2-28. Add the method implementation. The method should return a next to last element of the specified array.
+            // #2-28. Add the method implementation. The method should return a next to last element of the specified array.
             // Use index from end operator: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/ranges-indexes
-            throw new NotImplementedException();
+            return array[^2];
         }
 
         public static decimal GetLastElement(decimal[] array)
         {
-            // TODO #2-29. Add the method implementation. The method should return a last element of the specified array.
+            // #2-29. Add the method implementation. The method should return a last element of the specified array.
             // Use index from end operator: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/ranges-indexes
-            throw new NotImplementedException();
+            return array[^1];
         }
 
         public static decimal GetNextToLastElement(decimal[] array)
         {
-            // TODO #2-29. Add the method implementation. The method should return a next to last element of the specified array.
+            // #2-29. Add the method implementation. The method should return a next to last element of the specified array.
             // Use index from end operator: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/ranges-indexes
-            throw new NotImplementedException();
+            return array[^2];
         }
 
         public static decimal GetThirdElementFromEnd(decimal[] array)
         {
-            // TODO #2-30. Add the method implementation. The method should return a third element from the end of the specified array.
+            // #2-30. Add the method implementation. The method should return a third element from the end of the specified array.
             // Use index from end operator: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/ranges-indexes
-            throw new NotImplementedException();
+            return array[^3];
         }
 
         public static decimal GetFourthElementFromEnd(decimal[] array)
         {
-            // TODO #2-31. Add the method implementation. The method should return a forth element from the end of the specified array.
+            // #2-31. Add the method implementation. The method should return a forth element from the end of the specified array.
             // Use index from end operator: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/ranges-indexes
-            throw new NotImplementedException();
+            return array[^4];
         }
     }
 }
